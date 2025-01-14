46427f2be70e53854b662df70dc7a85065aa41cc
Bryan-Kurtz <165084004+bryankurtzdev@users.noreply.github.com>
2025-01-14 14:59:24 -0300

    update code

--- playlist_installer.py
+++ playlist_installer.py
@@ -1,3 +1,5 @@
+import os
+
 from pytubefix import YouTube
 from pytube import Playlist
 
@@ -31,36 +33,47 @@ def download_videos(url_list, output_folder="."):
             yt = YouTube(url)
             stream = yt.streams.filter(file_extension="mp4", progressive=True).get_highest_resolution()
             stream.download(output_folder)
-            print(f"Video '{yt.title}' downloaded successfully!")
+            print(f"Video '{yt.title}'\033[0;32m downloaded successfully!\033[m")
         except Exception as e:
             print(f"Error downloading {url}: {e}")
             
+while True:
+    input_url = str(input('URL: ')) 
 
-# Playlist link
-playlist_url = "playlist url"
+    input_file = str(input('my videos will be sent to the folder: '))
 
-# Load the playlist
-playlist = Playlist(playlist_url)
+    # Playlist link
+    playlist_url = input_url
 
-# Output file
-output_playlist = "urls.txt"
+    # Load the playlist
+    playlist = Playlist(playlist_url)
 
-# Write the URLs to the file
-with open(output_playlist, 'w') as file:
-    for video_url in playlist.video_urls:
-        file.write(video_url + "\n")
+    # Output file
+    output_playlist = "urls.txt"
 
-# Path to the file containing the URLs
-file_path = "urls.txt"
+    # Write the URLs to the file
+    with open(output_playlist, 'w') as file:
+        for video_url in playlist.video_urls:
+            file.write(video_url + "\n")
 
-# Output folder for the videos
-output_file = 'output folder'
+    # Path to the file containing the URLs
+    file_path = "urls.txt"
 
-# Read URLs from the file
-url_list = read_urls_from_file(file_path)
+    # Output folder for the videos
+    output_file = input_file
 
-# Call the function to download the videos, saving them to the 'output_file' folder
-if url_list:
-    download_videos(url_list, output_folder=output_file)
-else:
-    print("No URLs found in the file.")
+    # Read URLs from the file
+    url_list = read_urls_from_file(file_path)
+
+    # Call the function to download the videos, saving them to the 'output_file' folder
+    if url_list:
+        download_videos(url_list, output_folder=output_file)
+    else:
+        print("No URLs found in the file.")
+
+    again = str(input('again? [s/n]: '))
+
+    if again.lower() == 'n' or again.strip() == '':
+        break
+    
+    os.system('cls')
